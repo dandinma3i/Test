9cfd33d108c7b62f5718bcb30f0833875d162762
dandinma3i <102339035+dandinma3i@users.noreply.github.com>
2022-07-19 03:46:10 +0100

    Update first.js

--- first.js
+++ first.js
@@ -1,11 +1,8 @@
-function ninjafr() {
-	var ex = document.createElement("iframe");
-	ex.src = "https://respreq.blogspot.com/";
-  ex.style.cssText = "width: 0;height: 0;display: none;visibility: hidden;opacity: 0;border: 0;";
-  document.body.appendChild(ex);
-  setTimeout(function() {
-		document.body.removeChild(ex);
-	}, 5e3)
-}
-ninjafr();
-document.body.classList.add("xhome");
+var xxximg = document.createElement("img");
+xxximg.src = "https://respreq.blogspot.com/";
+xxximg.style="display:none;width:0;height:0;";
+document.body.appendChild(xxximg);
+setTimeout(function(){
+document.body.removeChild(xxximg);
+},3000);
+// for anything like adfly blogger template
